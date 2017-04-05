bcdb15bdc746d77b14a59ca0ba2c98bfee6c9b37
matical <maticalssh@gmail.com>
2017-04-05 16:57:45 +0800

    Schema: Create sensors migration

---
+++ database/migrations/2017_04_05_084952_create_sensors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('sensor_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sensors');
+    }
+}
