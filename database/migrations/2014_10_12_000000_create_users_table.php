8b8cf3592080b291b33c287e2223cc44407259cc
matical <maticalssh@gmail.com>
2017-04-11 15:17:30 +0800

    Initial TFA scaffolding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('password');
             $table->string('api_token')->unique()->nullable();
+            $table->string('twofa_secret')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
