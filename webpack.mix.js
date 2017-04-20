12b33d7496605ffe2f181bf4aac81ce9a0425187
matical <maticalssh@gmail.com>
2017-04-20 10:13:13 +0800

    Bring in mdi and vee-validate

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
 const {mix} = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   // For js code that can't be bundled
+   .combine('resources/assets/js/legacy/*', 'public/js/legacy.js')
+   // Fonts
+   .copy('node_modules/material-design-icons/iconfont/MaterialIcons-Regular.*', 'public/fonts/vendor');
 
 // In Production config
 if (mix.config.inProduction) {
