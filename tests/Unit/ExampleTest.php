bcf8ad8371e0e30cf86cf554b2d317bfbf7845f4
matical <maticalssh@gmail.com>
2017-05-02 17:42:59 +0800

    Add dummy test to avoid warnings

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -8,4 +8,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    /** @test */
+    public function it_returns_true()
+    {
+        return true;
+    }
 }
