13ab7957702ff0099b8b6e2814af091ca7dd9b31
matical <maticalssh@gmail.com>
2017-05-02 16:44:09 +0800

    Route scaffolding for users, throttle TFA

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,40 @@
 |
 */
 
+// Home
 Route::get('/', function () {
-    return view('welcome');
+    return view('main.index');
 });
 
+// Auth Routes
 Auth::routes();
 
 // Main Top-level routes
 Route::get('home', 'HomeController@index');
 Route::get('about', 'HomeController@about');
 
-// TFA
+
+/**
+ * User related routes
+ * (e.g. Configurations, settings, profiles etc) -- should be grouped
+ */
+Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
+    Route::get('profile', 'UserController@showProfile');
+    Route::group(['prefix' => 'session'], function () {
+        Route::get('/', 'Accounts\SessionController@view');
+    });
+});
+
+/**
+ * Two factor Authentication
+ * Token validation is throttled to 5 req/min.
+ */
 Route::group(['prefix' => 'TFA'], function () {
     Route::get('enable', 'TFAController@enable');
     Route::get('disable', 'TFAController@disable');
     Route::get('validate', 'Auth\LoginController@getValidateToken');
-    Route::post('validate', ['middleware' => 'throttle:5', 'uses' => 'Auth\LoginController@validateToken']);
-});
-
-Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
-    Route::get('profile', 'UserController@showProfile');
+    Route::post('validate', [
+        'middleware' => 'throttle:5',
+        'uses'       => 'Auth\LoginController@validateToken',
+    ]);
 });
