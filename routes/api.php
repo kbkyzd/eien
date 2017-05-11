544a32add119a8305833bd7b5890221b42aaa8b7
matical <maticalssh@gmail.com>
2017-05-11 17:58:22 +0800

    Routes for interacting with JSON bus datasets

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+/**
+ * v1 API Route
+ */
 
 Route::group(['prefix' => '/v1', 'middleware' => 'auth:api'], function () {
-    Route::get('test', 'HomeController@index');
-    Route::post('testing/{id}', function (Request $request, $id) {
-        return Response::json([
-            'id' => $id,
-            'token' => $request->input('api_token'),
-            'user' => Auth::guard('api')->id(),
-            'user2' => auth()->guard('api')->id()
-        ]);
+
+    Route::group(['prefix' => 'bus'], function () {
+
+        Route::group(['prefix' => 'raw'], function () {
+            Route::get('{file}', 'RawBusDataController@view');
+        });
+
+        /** Lookup */
+        Route::get('service/{busNo}', 'Api\BusController@service');
+        Route::get('stop/{stopNo}', 'Api\BusController@stop');
+        Route::get('stopService/{stopNo}', 'Api\BusController@stopService');
+        Route::get('busService/{mode}/{busNo}', 'Api\BusController@busService');
+
+        /** Fuzzy Search */
+        Route::group(['prefix' => 'search'], function () {
+            Route::get('service', 'Api\SearchController@searchServices');
+            Route::get('stop', 'Api\SearchController@searchStops');
+        });
     });
 });
 
-Route::get('user/{user}', function (eien\User $user) {
-    dd($user);
-});
+
