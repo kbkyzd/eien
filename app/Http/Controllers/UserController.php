619b58922829be10b04426c358eca329f623f69a
matical <maticalssh@gmail.com>
2017-05-13 18:47:55 +0800

    Initial user controller scaffold

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace eien\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function showProfile()
+    {
+        $last_activity = DB::table('sessions')
+                           ->where('user_id', Auth::user()->id)
+                           ->value('last_activity');
+
+        $diff = Carbon::createFromTimestamp($last_activity)
+                      ->diffForHumans();
+
+        return view('main.user.profile')->with('diff', $diff);
+    }
 }
