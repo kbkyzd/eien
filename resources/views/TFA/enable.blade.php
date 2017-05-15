5887234170c11f9e8c9ab36bfe395f81f8d2962d
matical <maticalssh@gmail.com>
2017-05-15 16:02:55 +0800

    Get rid of bootstrap fluff

--- resources/views/TFA/enable.blade.php
+++ resources/views/TFA/enable.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container spark-screen">
+    <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">2FA Secret Key</div>
+            <div class="col s12">
+                <h4>Secret Key</h4>
 
-                    <div class="panel-body">
-                        Open up your 2FA mobile app and scan the following QR barcode:
-                        <br/>
-                        <img alt="Image of QR barcode" src="{{ $image }}"/>
+                Open up your 2FA mobile app and scan the following QR barcode:
+                <br/>
+                <img alt="Image of QR barcode" src="{{ $image }}"/>
 
-                        <br/>
-                        If your 2FA mobile app does not support QR barcodes,
-                        enter in the following number: <code>{{ $secret }}</code>
-                        <br/><br/>
-                        <a href="{{ url('/home') }}">Go Home</a>
-                    </div>
-                </div>
+                <br/>
+                If your 2FA mobile app does not support QR barcodes,
+                enter in the following number: <code>{{ $secret }}</code>
+                <br/><br/>
+                <a class="btn waves-light waves-effect" href="{{ url()->previous() }}">Return</a>
             </div>
         </div>
     </div>
