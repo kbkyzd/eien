17e2f50559d882b048beb0c942bc8b6cc0ca7f46
matical <maticalssh@gmail.com>
2017-05-16 17:59:43 +0800

    Move fuzzy search core into it's own class

---
+++ app/Helpers/BusSearch.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace eien\Helpers;
+
+use wataridori\SFS\SimpleFuzzySearch;
+
+class BusSearch
+{
+    public function searchServices($query, Cached $cached)
+    {
+        $serviceObject = $cached->getAsArray('bus-services');
+
+        $sfs = new SimpleFuzzySearch($serviceObject['services'], ['name']);
+        $results = $sfs->search($query);
+
+        return $results;
+    }
+
+    public function searchStops($query, Cached $cached)
+    {
+        $stopObject = $cached->getAsArray('bus-stops');
+
+        $sfs = new SimpleFuzzySearch($stopObject, [
+            'name',
+            'no',
+        ]);
+        $results = $sfs->search($query);
+
+        return $results;
+    }
+}
