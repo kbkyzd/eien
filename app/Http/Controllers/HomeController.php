0e37f2caee6c8ad69674547dd50328108c3b4a7a
matical <maticalssh@gmail.com>
2017-05-22 03:21:23 +0800

    Cleanup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,8 @@
 
 namespace eien\Http\Controllers;
 
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
-
     /**
      * Show the application dashboard.
      *
